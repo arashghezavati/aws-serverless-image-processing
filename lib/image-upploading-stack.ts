1672cefecc8d47075bded2ba3edc1ae2cc28aba6
Arash <76617498+arashghezavati@users.noreply.github.com>
2024-09-19 12:10:57 -0700

    Added API Gateway, S3 and Lambda for Image Uploadin

--- lib/image-upploading-stack.ts
+++ lib/image-upploading-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps} from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3'
 
 export class ImageUpploadingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Define S3 bucket for image storage
+    const imageBucket = new s3.Bucket(this, 'imageBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImageUpploadingQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const uploadLambda = new lambda.Function(this, 'UploadImageLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler:'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment : {
+        BUCKET_NAME : imageBucket.bucketName
+      }
+    });
+
+    imageBucket.grantPut(uploadLambda);
+
+    const api = new apigateway.RestApi(this, 'ImageUploadApi' , {
+      restApiName:'Image Upload Service',
+      description: 'This service uploads images to S3 via API Gateway.'
+    });
+    const uploadIntegration = new apigateway.LambdaIntegration(uploadLambda);
+    api.root.addMethod('POST', uploadIntegration)
   }
 }
